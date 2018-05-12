6d32cf4c5874524d1b5af33265a7685ae178e23e
AdamStan <adam15stan@gmail.com>
2018-05-12 17:32:48 +0200

    Adding class library: EncjeUczelnii and console app: TestowanieNHibernate

---
+++ EncjeUczelnii/Tutor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncjeUczelnii
+{
+    public class Tutor
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public String surname { get; set; }
+        public String postCode { get; set; }
+        public String street { get; set; }
+        public String number { get; set; }
+        //fields need to login:
+        //string are immutables, we have to.
+        public String username{ get; set; }
+        public String password{ get; set; }
+
+        public Tutor()
+        {
+            this.name = "xyz";
+            this.surname = "abc";
+        }
+    }
+}
---
+++ TestowanieNHibernate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System.Reflection;
+using EncjeUczelnii;
+using System.Data;
+
+namespace TestowanieNHibernate
+{
+    class Program
+    {
+        /*
+         * Co trzeba miec aby to dzialalo?
+         * 1. DataSource trzeba zmienic na swoje
+         * 2. InitialCatalog -> nazwa bazy danych
+         * 3. 
+         */
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration()
+                .Configure("D:\\Projekty\\C#\\Uczelnia\\TestowanieNHibernate\\hibernate.cfg.xml")
+                .AddFile("D:\\Projekty\\C#\\Uczelnia\\EncjeUczelnii\\Tutor.hbm.xml");
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sefact = cfg.BuildSessionFactory();
+            using (var session = sefact.OpenSession())
+            {
+
+                using (var tx = session.BeginTransaction())
+                {
+                    Tutor buff = new Tutor();
+                    session.Save(buff);
+                    tx.Commit();
+                }
+            }
+            Console.WriteLine("THE END - sprawdź tabele");
+        }
+    }
+}
